f615915709e86e9fc87d094b90430d3f7bf8c9dd
mcfilu <filip.1olszewski@gmail.com>
2023-11-09 13:40:53 +0100

    creaton of hours component from the homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
 import Image from 'next/image'
 import Loader from './components/navigation/loader'
 import Menu from './components/navigation/menu'
+import Footer from './components/footer'
+import Header from './components/home/header'
+import Hours from './components/home/hours'
+
+
 
 export default function Home() {
   return (
    <div>
     {/* <Loader/> */}
-    <Menu/>
+    {/* <Menu/> */}
+    <Header/>
+    <Hours/>
+    <Footer/>
    </div>
   )
 }
