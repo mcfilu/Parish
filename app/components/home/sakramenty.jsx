c1e6167affb9d7546f4efb5bf62b63969604c489
mcfilu <filip.1olszewski@gmail.com>
2023-11-11 20:47:48 +0100

    creation of part of the sacrametns page from the home page

--- app/components/home/sakramenty.jsx
+++ app/components/home/sakramenty.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Sakramenty = () => {
   return (
-    <div>Sakramenty</div>
+    <div className='flex flex-col w-screen h-[60vh] bg-white'>
+        <h2>Services</h2>
+        <div className=''>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+
+    </div>
   )
 }
 
