d6192d8c006338be6402caad7b189ad4827dfd6d
mcfilu <filip.1olszewski@gmail.com>
2023-11-15 17:20:23 +0100

    creation of episodes component from the kazania page

--- app/aktualnosci/kazania/episodes.jsx
+++ app/aktualnosci/kazania/episodes.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 const Episodes = () => {
   return (
     <div className='w-screen flex flex-col'>
-        <div>
-
+        <div className='flex flex-row w-full h-[10vh] '>
+            <div className='bg-dark text-white'>
+                2023
+            </div>
+            <div border-4>
+                2022
+            </div>
         </div>
         <div>
-            
+
         </div>
     </div>
   )
--- app/components/navigation/menu.jsx
+++ app/components/navigation/menu.jsx
@@ -7,12 +7,16 @@ import { useHeaderVisibility } from '@/app/HeaderVisibilityContext'
 
 const Menu = () => {
     const { isHeaderVisible, toggleMenu } = useHeaderVisibility();
-    const {menuElement, setElement} = useState('');
+    const [menuElement, setElement] = useState('');
+    const closeReset = () => {
+        setElement('');
+        toggleMenu();
+    }
     console.log(isHeaderVisible, toggleMenu);
   return (
     <div className={`fixed z-50 overflow-hidden w-screen h-screen bg-dark text-white px-[10vw] pb-[10vh] ${isHeaderVisible ? 'fixed' : 'hidden'}`}>
         <div className='flex flex-col'>
-            <div className={`flex flex-row justify-end h-auto mt-[5vh] mb-[10vh]`}  onClick={() => toggleMenu()}>
+            <div className={`flex flex-row justify-end h-auto mt-[5vh] mb-[10vh]`}  onClick={() => closeReset()}>
                 <Image src="/close.png" alt="close icon"  width={20} height={10} objectFit='contain'></Image>
                 <p className='flex align-center justify-center ml-[10px]'>ZAMKNIJ</p>
                 
@@ -29,20 +33,20 @@ const Menu = () => {
                     <p>parafiachlodna@gmail.com</p>
                     <p>22 620 37 47</p>
                 </div>
-                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%]`}>
-                    <Link href="/nabozenstwa" className='text-4'>Nabozenstwa</Link>
+                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%] ${menuElement == '' ? '' : 'hidden'}`}>
+                    <Link href="/nabozenstwa">Nabozenstwa</Link>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
-                    <Link href="">Aktualności</Link>
+                    <a className='mt-[1vh] mb-[1vh] border-1' onClick={() => setElement('aktualnosci')}>Aktualnosci</a>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
-                    <Link href="">Parafia</Link>
+                    <a className='mt-[1vh] mb-[1vh] border-1' onClick={() => setElement('parafia')}>Parafia</a>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
-                    <Link href="">Sakramenty</Link>
+                    <a className='mt-[1vh] mb-[1vh] border-1' onClick={() => setElement('sakramenty')}>Sakramenty</a>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
-                    <Link href="">Wspólnoty</Link>
+                    <a className='mt-[1vh] mb-[1vh] border-1' onClick={() => setElement('wspolnoty')}>Wspolnoty</a>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
                 </div>
 
-                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%]`}>
+                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%] ${menuElement == 'aktualnosci' ? '' : 'hidden'}`}>
                     <Link href="" className='text-4'>Aktualnosci</Link>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
                     <Link href="">Ogloszenia</Link>
@@ -54,6 +58,53 @@ const Menu = () => {
                     <Link href="">Ciekawostki Kosciola</Link>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
                     <Link href="">Kazania Proboszcza</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                </div>
+
+                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%] ${menuElement == 'parafia' ? '' : 'hidden'}`}>
+                    <Link href="" className='text-4'>Histora</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Galeria</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Ciekawosti Parafian</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Duszpasterze</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Teren Parafii</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Kancelaria</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                </div>
+
+                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%] ${menuElement == 'sakramenty' ? '' : 'hidden'}`}>
+                    <Link href="/sakramenty/chrzest" className='text-4'>Chrzest</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/komunia">Komunia</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/bierzmowanie">Bierzmowanie</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/pokuta">Pokuta</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/malzenstwo">Malzenstwo</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/kaplanstwo">Kaplanstwo</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/sakramenty/namaszczenie">Namaszczenie Chorych</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                </div>
+
+                <div className={`flex flex-col w-[65%] text-white text-[30px] justify-center ml-[10%] ${menuElement == 'wspolnoty' ? '' : 'hidden'}`}>
+                    <Link href="/wspolnoty/schola" className='text-4'>Schola</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/wspolnoty/schola-dziecieca">Schola Dzieci</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/wspolnoty/caritas">Caritas</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="/wspolnoty/ministranci">Ministranci</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Rada Parafialna</Link>
+                    <hr className='mt-[1vh] mb-[1vh] border-1'/>
+                    <Link href="">Kolo Zywego Rozanca</Link>
                     <hr className='mt-[1vh] mb-[1vh] border-1'/>
                 </div>
                 
