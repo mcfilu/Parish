7c9b9ad950e908cb7cb2250b881eb9ce598fa045
mcfilu <filip.1olszewski@gmail.com>
2023-11-16 00:39:56 +0100

    adjusting the proboszcz component to eslint rules

--- app/components/home/proboszcz.jsx
+++ app/components/home/proboszcz.jsx
@@ -6,8 +6,8 @@ const Proboszcz = () => {
         <div className='flex flex-col w-[50%] h-full justify-center pl-[20%]'>
             <h1 className='text-[30px] text-bold'>Leszek Slipek</h1>
             <h2 className='text-[25px]'>Ksiądz Proboszcz</h2>
-            <p className='mt-[5vh]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
-            <p className='mt-[2vh]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here',</p>
+            <p className='mt-[5vh]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here,</p>
+            <p className='mt-[2vh]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content her,</p>
             <div className='h-[20%] w-[30%] relative'>
                 <Image alt="podpis" src="/signing.png" fill={true} objectFit='contain'/>
             </div>
