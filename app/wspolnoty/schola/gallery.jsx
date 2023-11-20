19574b5af27d4a074cbc412405d0639f5ffe6160
mcfilu <filip.1olszewski@gmail.com>
2023-11-20 14:45:01 +0100

    creatin of full schola page

--- app/wspolnoty/schola/gallery.jsx
+++ app/wspolnoty/schola/gallery.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <div className='w-screen px-[5vw] py-[10vh] grid grid-cols-2 h-[90vh] gap-4'>
+        <div className='relative h-full w-full'>
+            <Image src="/schola9.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+        </div>
+        <div className='grid grid-cols-2 h-full gap-4'>
+            <div className='relative'>
+                <Image src="/schola3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            </div>
+            <div className='relative h-full w-full'>
+                <Image src="/schola6.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            </div>
+            <div className='relative h-full w-full'>
+                <Image src="/schola8.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            </div>
+            <div className='relative h-full w-full'>
+                <Image src="/schola10.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
--- app/wspolnoty/schola/intro.jsx
+++ app/wspolnoty/schola/intro.jsx
@@ -3,15 +3,15 @@ import Image from 'next/image'
 
 const Intro = () => {
   return (
-    <div className='w-screen flex flex-row h-[80vh]'>
+    <div className='w-screen flex flex-row h-[90vh] px-[5vw] py-[10vh] bg-white'>
         <div className='flex w-1/2 h-full relative'>
-            <Image alt="zdjecie schola" src="/schola4.jpeg" fill objectFit='cover'/>
+            <Image className="pr-[2vw]" alt="zdjecie schola" src="/schola4.jpeg" fill objectFit='cover'/>
         </div>
-        <div>
-            <h3></h3>
-            <p></p>
-            <p></p>
-            <p></p>
+        <div className='pl-[2vw] flex flex-col justify-center w-1/2'>
+            <h3 className='text-[40px] font-header2'>Tytul do Dodania</h3>
+            <p className='mt-[4vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+            <p className='mt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+            <p className='mt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
         </div>
     </div>
   )
