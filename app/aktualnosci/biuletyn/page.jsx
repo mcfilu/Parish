fcbc0130d0c0f022fc2aa8af3fd49e34aaa5d56b
mcfilu <filip.1olszewski@gmail.com>
2023-11-21 14:47:16 +0100

    styling the kazania page, fully fanctional and creation of the files for the biuletyn page

--- app/aktualnosci/biuletyn/page.jsx
+++ app/aktualnosci/biuletyn/page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './header'
 import Footer from '@/app/components/footer'
+import Files from './files'
 
 const Page = () => {
   return (
     <div>
         <Header/>
-
+        <Files/>
         <Footer/>
     </div>
   )
--- app/aktualnosci/kazania/episodes.jsx
+++ app/aktualnosci/kazania/episodes.jsx
@@ -20,16 +20,16 @@ const Episodes = () => {
     }
     // const years = Array.from(new Set(data.data.map(item => item.attributes.data.split('-')[0]))).sort().reverse();
     const [currentYear, changeYear] = useState(new Date().getFullYear());
-    
-    console.log('yalla');
+    console.log(currentYear);
+    console.log(years);
   return (
 
     <div className='w-screen flex flex-col py-[10vh]'>
         
-        <div className='flex flex-row w-full space-x-4 h-[5vh] items-center justify-center '>
+        <div className='flex flex-row w-full space-x-4 h-[5vh] pb-[5vh] items-center justify-center '>
             {years && years.map(item => (
                 // eslint-disable-next-line react/jsx-key
-                <button onClick={item => changeYear(item)} className={`bg-dark text-white p-4 ${item == currentYear ? 'bg-dark text-white' : 'bg-white text-black'}`}>
+                <button onClick={() => changeYear(item)} className={`bg-dark text-white p-4 ${item == currentYear ? 'bg-dark text-white' : 'bg-white text-black'}`}>
                     {item}
                 </button>
             ))}
@@ -44,24 +44,25 @@ const Episodes = () => {
             </button> */}
         </div>
         
+        
+        <div  className={`w-full flex flex-col items-center justify-center `}>
         {data && data.data.map((item) => (
-        <div key={item.id} className='w-full flex flex-col items-center justify-center'>
-            <div className='w-[60%] h-[40vh] flex flex-row'>
-                <div className='flex flex-col h-full w-1/2'>
-                    <h2>{item.attributes.tytul}</h2>
-                    <h2>{item.attributes.data}</h2>
-                    <p>{item.attributes.opis}</p>
-                    <button className='p-4 bg-dark text-white'>Obejrzyj</button>
+            <div key={item.id} className={`my-[5vh] w-[60%] h-[40vh] flex flex-row ${item.attributes.data.split('-')[0] == currentYear ? '': 'hidden'}`}>
+                <div className='flex flex-col h-full w-1/2 justify-center'>
+                    <h2 className='text-[20px] font-header2'>{item.attributes.data}</h2>
+                    <h2 className='text-[40px] font-header2'>{item.attributes.tytul}</h2>
+                    
+                    <p className='mt-[2vh] '>{item.attributes.opis}</p>
+                    <button className='mt-[4vh] p-4 bg-dark text-white w-[30%]'>Obejrzyj</button>
                 </div>
                 <div className='h-full w-1/2 relative'>
                     <Image fill objectFit="cover" alt='zdjecie kazanie' src={'http://127.0.0.1:1337' + item.attributes.zdjecie.data.attributes.url}></Image>
                 </div>
             </div>
-            <div className='w-[60%] h-[40vh] '>
-
-            </div>
+        
+            ))}
         </div>
-        ))}
+        
     </div>
   )
 }
