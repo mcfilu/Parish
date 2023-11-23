33e0a7c11e5d47e8f7e7e95c4ff25ded36b6ead1
mcfilu <filip.1olszewski@gmail.com>
2023-11-23 23:30:52 +0100

    addition of the all components along the page.jsx for all remaining subpages in the sakramenty directory

---
+++ app/sakramenty/bierzmowanie/detailed.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Detailed = () => {
+  return (
+    <div className='w-screen h-[30vh] px-[5vw] flex flex-row bg-white'>
+        <div className='h-full w-1/2 pr-[5vw] items-center flex'>
+            <h2 className='text-[40px] font-header2'>Long heading is what you see here in this feature section</h2>
+        </div>
+        <div className='h-full w-1/2 flex flex-col items-center justify-center'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+            <p className='mt-[3vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Detailed
---
+++ app/sakramenty/bierzmowanie/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './header'
+import Footer from '@/app/components/footer'
+import Intro from './intro'
+import Detailed from './detailed'
+import Gallery from './gallery'
+import Day from './day'
+import Banner from './banner'
+import Faqs from './faqs'
+import Documents from './documents'
+
+const Page = () => {
+  return (
+    <div className='bg-white'>
+        <Header/>
+        <Intro/>
+        <Detailed/>
+        <Gallery/>
+        <Day/>
+        <Banner/>
+        <Faqs/>
+        <Documents/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Page
