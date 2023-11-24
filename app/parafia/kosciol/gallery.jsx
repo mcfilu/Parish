cbb9ac93a132809c3a96cb6a25f118a1514c2e32
mcfilu <filip.1olszewski@gmail.com>
2023-11-25 00:19:19 +0100

    creation of the gallery page, both general and id specific

--- app/parafia/kosciol/gallery.jsx
+++ app/parafia/kosciol/gallery.jsx
@@ -4,20 +4,20 @@ const Gallery = () => {
   return (
     <div className='w-screen px-[5vw] py-[10vh] grid grid-cols-2 h-[90vh] gap-4 bg-white'>
         <div className='relative h-full w-full'>
-            <Image src="/chrzest_gallery1.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            <Image src="/kosciol5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
         </div>
         <div className='grid grid-cols-2 h-full gap-4'>
             <div className='relative'>
-                <Image src="/chrzest_gallery2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/nawa2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
 
         </div>
