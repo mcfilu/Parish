3dc5238969779408c43da96b1fed8b9932a4a0f0
mcfilu <filip.1olszewski@gmail.com>
2023-11-24 10:14:43 +0100

    changes to kancelaria header and addition of the components remaining from that page;

--- app/parafia/kancelaria/header.jsx
+++ app/parafia/kancelaria/header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <div className='flex flex-col w-screen h-screen'>
         <div className='w-full h-[80%] relative'>
-            <Image alt="ksiadz proboszcz kazanie" fill objectFit='cover' src="/proboszcz_kazania.jpeg"/>
+            <Image alt="kancelaria parafialna" fill objectFit='cover' src="/kancelaria.png"/>
         </div>
         <div className='bg-white w-full h-[20%] flex flex-row px-[5vw] pb-[3vh] pt-[1vh] items-center justify-between'>
-            <h1 className='text-[45px]'>Kazania Proboszcza</h1>
+            <h1 className='text-[45px]'>Kancelaria Parafialna</h1>
             <div className='w-[50%] h-full flex flex-col justify-center'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
                 <button className='bg-dark text-white px-4 py-3 w-[20%] mt-[2vh]'>Zobacz Wiecej</button>
