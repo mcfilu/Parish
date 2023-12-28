bde560dfc5891abf766e4cbf2ccfa7e6da1b97bc
mcfilu <filip.1olszewski@gmail.com>
2023-12-28 22:38:07 +0100

    updating the images domains to server from new server, not only locally

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {}
 
 module.exports = {
     images: {
-      domains: ['127.0.0.1'],
+      domains: ['127.0.0.1', '52.56.212.148'],
     },
     ignoreDuringBuilds: true,
     // ... any other existing configurations
