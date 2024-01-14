925dcd7bda3bef43bf6517444fc347ffebf74bd2
mcfilu <filip.1olszewski@gmail.com>
2024-01-14 20:01:57 +0100

    adding the sacraments

--- app/components/home/sakramenty.jsx
+++ app/components/home/sakramenty.jsx
@@ -4,90 +4,139 @@ import Link from 'next/link'
 
 const Sakramenty = () => {
   return (
-    <div className='flex flex-col w-full pb-[5vh] bg-white'>
-        <h2 className='text-[40px] text-center my-[5vh]'>Sakramenty</h2>
-        <div className='flex flex-col w-full '>
-            <div className='flex flex-row h-[30vh] w-full justify-center'>
-                <Link href="/sakramenty/chrzest">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1  '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/baptism.png"/>
+    <div className='w-full h-[50vh] flex relative'>
+        <Image src="/wspolnoty.jpeg" alt="tlo wspolnoty" fill={true} /> 
+        <div className='z-10 w-full h-full py-[8vh] px-[8vw] flex flex-col justify-center items-center'>
+            <h2 className='text-white text-[50px] font-header2'>Sakramenty</h2>
+            {/* <p className='text-white mt-[3vh]'>W naszym kosciel istnieje nie od dzis wiele roznych zgromadzen ktore zajmuja sie blah blah przerozny;mji rzeczami. Juz dzis mozesz dolaczyc i sie zaangazowac.</p> */}
+            <div className='mt-[5vh] flex flex-row justify-center text-white font-header2 text-[30px] gap-10'>
+                <Link href="/wspolnoty/caritas">
+                <div className='flex h-[15vh] w-[15vh] hover:bg-white hover:bg-opacity-30 flex-col justify-center items-center mx-[1vw]'>
+                    <div className='h-[60%] relative w-[50%]'>
+                        <Image src="/handshake.png" alt="ikonka wspolnoty" fill objectFit='contain'/>
                     </div>
-                    <h3 className='text-[20px] text-center mt-[2vh] '>Chrzest Swiety</h3>
-                    <p className='text-center mt-[1vh]'>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+                    <p className='text-center text-bold'>CHRZEST</p>
                 </div>
                 </Link>
-                <Link href="/sakramenty/komunia">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/communion.png"/>
+                <Link href="/wspolnoty/schola">
+                <div className='flex h-[15vh] w-[15vh] hover:bg-white hover:bg-opacity-30 flex-col justify-center items-center mx-[1vw]'>
+                    <div className='h-[60%] relative w-[50%]'>
+                        <Image src="/handshake.png" alt="ikonka wspolnoty" fill objectFit='contain'/>
                     </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Eucharystia</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+                    <p className='text-center text-bold '>KOMUNIA</p>
                 </div>
                 </Link>
-                <Link href="/sakramenty/bierzmowanie">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/bierzmowanie.png"/>
+                <Link href="/wspolnoty/schola-dziecieca">
+                <div className='flex h-[15vh] w-[15vh] hover:bg-white hover:bg-opacity-30 flex-col justify-center items-center mx-[1vw]'>
+                    <div className='h-[60%] relative w-[50%]'>
+                        <Image src="/handshake.png" alt="ikonka wspolnoty" fill objectFit='contain'/>
                     </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Bierzmowanie</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+                    <p className='text-center text-bold'>BIERZMOWANIE</p>
                 </div>
                 </Link>
-            </div>
-            <div className='flex flex-row h-[30vh] w-full justify-center'>
-                <Link href="/sakramenty/pokuta">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/contrition.png"/>
+                <Link href="/wspolnoty/ministranci">
+                <div className='flex h-[15vh] w-[15vh] hover:bg-white hover:bg-opacity-30 flex-col justify-center items-center mx-[1vw]'>
+                    <div className='h-[60%] relative w-[50%]'>
+                        <Image src="/handshake.png" alt="ikonka wspolnoty" fill objectFit='contain'/>
                     </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Pokuta</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+                    <p className='text-center text-bold'>MAŁZEŃSTWO</p>
                 </div>
                 </Link>
-                <Link href="/sakramenty/malzenstwo">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/wedding-rings.png"/>
+                <Link href="/wspolnoty/rada">
+                <div className='flex h-[15vh] w-[15vh] hover:bg-white hover:bg-opacity-30 flex-col justify-center items-center mx-[1vw]'>
+                    <div className='h-[60%] relative w-[50%]'>
+                        <Image src="/handshake.png" alt="ikonka wspolnoty" fill objectFit='contain'/>
                     </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Malzenstwo</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
-                </div>
-                </Link>
-                {/* <Link href="/sakramenty/kaplanstwo">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/pope.png"/>
-                    </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Kaplanstwo</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
-                </div>
-                </Link> */}
-                <Link href="/sakramenty/namaszczenie">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/anointing.png"/>
-                    </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Namaszczenie Chorych</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+                    <p className='text-center text-bold'>NAMASZCZENIE CHORYCH</p>
                 </div>
                 </Link>
             </div>
-            {/* <div className='flex flex-row h-[30vh] w-full justify-center'>
-                <Link href="/sakramenty/namaszczenie">
-                <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
-                    <div className='w-full h-[35%] relative'>
-                        <Image alt="ikonka " fill objectFit="contain" src="/anointing.png"/>
-                    </div>
-                    <h3 className='text-[20px] text-center mt-[2vh]'>Namaszczenie Chorych</h3>
-                    <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
-                </div>
-                </Link>
-            </div> */}
         </div>
-
     </div>
+    // <div className='flex flex-col w-full pb-[5vh] bg-white'>
+    //     <h2 className='text-[40px] text-center my-[5vh]'>Sakramenty</h2>
+    //     <div className='flex flex-col w-full '>
+    //         <div className='flex flex-row h-[30vh] w-full justify-center'>
+    //             <Link href="/sakramenty/chrzest">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1  '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/baptism.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh] '>Chrzest Swiety</h3>
+    //                 <p className='text-center mt-[1vh]'>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //             <Link href="/sakramenty/komunia">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/communion.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Eucharystia</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //             <Link href="/sakramenty/bierzmowanie">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/bierzmowanie.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Bierzmowanie</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //         </div>
+    //         <div className='flex flex-row h-[30vh] w-full justify-center'>
+    //             <Link href="/sakramenty/pokuta">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/contrition.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Pokuta</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //             <Link href="/sakramenty/malzenstwo">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/wedding-rings.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Malzenstwo</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //             {/* <Link href="/sakramenty/kaplanstwo">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/pope.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Kaplanstwo</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link> */}
+    //             <Link href="/sakramenty/namaszczenie">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/anointing.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Namaszczenie Chorych</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //         </div>
+    //         {/* <div className='flex flex-row h-[30vh] w-full justify-center'>
+    //             <Link href="/sakramenty/namaszczenie">
+    //             <div className='flex flex-col h-full w-[17vw] mx-[5vw] border-4 border-white hover:border-black p-1 '>
+    //                 <div className='w-full h-[35%] relative'>
+    //                     <Image alt="ikonka " fill objectFit="contain" src="/anointing.png"/>
+    //                 </div>
+    //                 <h3 className='text-[20px] text-center mt-[2vh]'>Namaszczenie Chorych</h3>
+    //                 <p className='text-center mt-[1vh] '>Jest to sakrament, w którym ciężko chory otrzymuje przez namaszczenie olejem św. i modlitwę kapłana szczególne łaski, ulgę w cierpieniu, a niekiedy przywrócenie zdrowia.</p>
+    //             </div>
+    //             </Link>
+    //         </div> */}
+    //     </div>
+
+    // </div>
   )
 }
 
