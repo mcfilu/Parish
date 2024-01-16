64cf1561a6e20885309b80c75e375910bc3a0d87
mcfilu <filip.1olszewski@gmail.com>
2024-01-16 20:07:18 +0100

    changing the banner and day components from chrzest to be mobile friendly

--- app/sakramenty/chrzest/day.jsx
+++ app/sakramenty/chrzest/day.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Day = () => {
   return (
-    <div className='flex flex-col px-[15vw] bg-white'>
+    <div className='flex flex-col px-[5vw] md:px-[15vw] py-[10vh] bg-white text-black'>
         <h2 className='text-[5.5vh] font-header2 text-center'>W DNIU CHRZTU SWIETEGO</h2>
         <p className=' mt-[3vh]'>Odio felis sagittis, morbi feugiat tortor vitae feugiat fusce aliquet. Nam elementum urna nisi aliquet erat dolor enim. Ornare id morbi eget ipsum. Aliquam senectus neque ut id eget consectetur dictum. Donec posuere pharetra odio consequat scelerisque et, nunc tortor. Nulla adipiscing erat a erat. Condimentum lorem posuere gravida enim posuere cursus diam.</p>
         <p className=' mt-[2vh]'>Odio felis sagittis, morbi feugiat tortor vitae feugiat fusce aliquet. Nam elementum urna nisi aliquet erat dolor enim. Ornare id morbi eget ipsum. Aliquam senectus neque ut id eget consectetur dictum. Donec posuere pharetra odio consequat scelerisque et, nunc tortor. Nulla adipiscing erat a erat. Condimentum lorem posuere gravida enim posuere cursus diam.</p>
