f893605f5966f099e71457e435a1531ac62bcf40
mcfilu <filip.1olszewski@gmail.com>
2024-01-17 19:26:59 +0100

    making the duszpasterze page to be fully responsive

--- app/parafia/duszpasterze/pozostali.jsx
+++ app/parafia/duszpasterze/pozostali.jsx
@@ -4,31 +4,40 @@ import Image from 'next/image'
 const Pozostali = () => {
   return (
     <div className='w-full flex flex-col px-[5vw] py-[10vh] bg-white'>
-        <div className='w-full h-[50vh] flex flex-row'>
-            <div className='w-1/2 h-full relative'>
+        <div className='w-full  flex flex-row'>
+            <div className='hidden md:flex w-1/2 h-[50vh] relative'>
                 <Image className='pr-[5vw]' alt="zdjecie wikariusza" src="/ksiadz_andrzej.jpeg" fill objectFit='cover'/>
             </div>
-            <div className='flex flex-col w-1/2 justify-center pl-[3vw]'>
+            <div className='flex flex-col w-full md:w-1/2 justify-center md:pl-[3vw]'>
                 <h2 className='text-[4.5vh] font-header2'>Ks. Andrzej Ostrowski</h2>
+                <div className='flex md:hidden w-full h-[40vh] relative my-[2vh]'>
+                    <Image className='' alt="zdjecie wikariusza" src="/ksiadz_andrzej.jpeg" fill objectFit='cover'/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
             </div>
         </div>
-        <div className='w-full h-[50vh] flex flex-row mt-[5vh]'>
+        <div className='w-full  flex flex-row mt-[5vh]'>
             
-            <div className='flex flex-col w-1/2 justify-center pr-[3vw]'>
+            <div className='flex flex-col w-full md:w-1/2 justify-center pr-[3vw]'>
                 <h2 className='text-[4.5vh] font-header2'>Ks. Pawel Powierza</h2>
+                <div className='flex md:hidden w-full h-[40vh] relative my-[2vh]'>
+                    <Image className='' alt="zdjecie wikariusza" src="/ksiadz_pawel2.jpeg" fill objectFit='cover'/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
             </div>
-            <div className='w-1/2 h-full relative'>
+            <div className='hidden md:flex w-1/2 h-[50vh] relative'>
                 <Image className='pl-[5vw]' alt="zdjecie wikariusza" src="/ksiadz_pawel2.jpeg" fill objectFit='cover'/>
             </div>
         </div>
-        <div className='w-full h-[50vh] flex flex-row mt-[5vh]'>
-            <div className='w-1/2 h-full relative'>
+        <div className='w-full  flex flex-row mt-[5vh]'>
+            <div className='hidden md:flex w-1/2 h-[50vh] relative'>
                 <Image className='pr-[5vw]' alt="zdjecie wikariusza" src="/ksiadz_zbigniew.jpeg" fill objectFit='cover'/>
             </div>
-            <div className='flex flex-col w-1/2 justify-center pl-[3vw]'>
+            <div className='flex flex-col w-full md:w-1/2 justify-center md:pl-[3vw]'>
                 <h2 className='text-[4.5vh] font-header2'>Ks. Zbigniew Badowski</h2>
+                <div className='flex md:hidden w-full h-[40vh] relative my-[2vh]'>
+                    <Image className='' alt="zdjecie wikariusza" src="/ksiadz_zbigniew.jpeg" fill objectFit='cover'/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
             </div>
         </div>
--- app/parafia/duszpasterze/proboszcz.jsx
+++ app/parafia/duszpasterze/proboszcz.jsx
@@ -3,17 +3,17 @@ import Image from 'next/image'
 
 const Proboszcz = () => {
   return (
-    <div className='w-full flex flex-col bg-white px-[5vw]'>
-        <div className='w-full h-[70vh] relative'>
+    <div className='w-full flex flex-col bg-white px-[5vw] py-[10vh]'>
+        <div className='w-full h-[50vh] md:h-[60vh] relative'>
             <Image alt="ksiadz proboszcz" src="/leszek_slipek.jpeg" fill objectFit='cover'/>
         </div>
-        <div className='flex flex-row w-full justify-between mt-[5vh]'>
+        <div className='flex flex-col md:flex-row w-full justify-between mt-[5vh]'>
             <div className='min-w-[30%]'>
                 <h2 className='text-[4.5vh] font-header2'>Ks. Proboszcz</h2>
                 <h2 className='text-[4.5vh] font-header2'>Leszek Slipek</h2>
             </div>
-            <div className='max-w-[40%]'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
+            <div className='w-full md:max-w-[40%]'>
+                <p className='mt-[2vh] md:mt-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla.</p>
             </div>
         </div>
         
