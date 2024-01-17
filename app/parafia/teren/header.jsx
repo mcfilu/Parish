980abddec49a124d41dcd2c1021b7cfcba99865f
mcfilu <filip.1olszewski@gmail.com>
2024-01-17 19:20:50 +0100

    changing the folder name from obszar to teren as to fit the domain name set in menu plus changin that to be responsive"

--- app/parafia/teren/header.jsx
+++ app/parafia/teren/header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <div className='flex flex-col w-full h-screen'>
         <div className='w-full h-full relative'>
-            <Image alt="ksiadz proboszcz kazanie" fill objectFit='cover' src="/duszpasterze.jpeg"/>
+            <Image alt="ksiadz proboszcz kazanie" fill objectFit='cover' src="/teren_parafii.jpeg"/>
         </div>
         <div className='bg-white w-full flex flex-col md:flex-row px-[5vw] md:py-[2vh] pt-[0] pb-[1vh] items-start md:items-center justify-between'>
-            <h1 className='text-[55px] font-header2 font-bold text-black'>Duszpasterze</h1>
+            <h1 className='text-[55px] font-header2 font-bold text-black'>Teren Parafii</h1>
             <div className='w-full md:w-[50%] h-full flex flex-col justify-center items-start'>
                 <p className='text-black text-[1.7vh]'>Prezentujemy naszą parafie w czasach dzisiejszych, zapraszamy do zapoznania sie ze szczegółami. Zobacz co kościół ma do zaoferowania.</p>
                 <button className='bg-dark text-white px-4 py-3 mt-[1vh]'>Zobacz Więcej</button>
