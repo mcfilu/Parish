3c3e4205a16e22e87ef69f64314f843393e63777
mcfilu <filip.1olszewski@gmail.com>
2024-01-17 19:10:21 +0100

    updating kosciol page to be fully responsive

--- app/parafia/kosciol/gallery.jsx
+++ app/parafia/kosciol/gallery.jsx
@@ -1,8 +1,37 @@
+// import React from 'react'
+// import Image from 'next/image'
+// const Gallery = () => {
+//   return (
+//     <div className='w-full px-[5vw] py-[10vh] grid grid-cols-2 h-[90vh] gap-4 bg-white'>
+//         <div className='relative h-full w-full'>
+//             <Image src="/kosciol5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+//         </div>
+//         <div className='grid grid-cols-2 h-full gap-4'>
+//             <div className='relative'>
+//                 <Image src="/kosciol4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+//             </div>
+//             <div className='relative h-full w-full'>
+//                 <Image src="/kosciol2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+//             </div>
+//             <div className='relative h-full w-full'>
+//                 <Image src="/kosciol3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+//             </div>
+//             <div className='relative h-full w-full'>
+//                 <Image src="/nawa2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+//             </div>
+
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Gallery
+
 import React from 'react'
 import Image from 'next/image'
 const Gallery = () => {
   return (
-    <div className='w-full px-[5vw] py-[10vh] grid grid-cols-2 h-[90vh] gap-4 bg-white'>
+    <div className='w-full px-[5vw] grid grid-cols-1 md:grid-cols-2 h-[90vh] py-[10vh] md:h-[80vh] gap-4 bg-white'>
         <div className='relative h-full w-full'>
             <Image src="/kosciol5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
         </div>
@@ -11,10 +40,10 @@ const Gallery = () => {
                 <Image src="/kosciol4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/kosciol2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/kosciol3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
                 <Image src="/nawa2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
--- app/parafia/kosciol/places.jsx
+++ app/parafia/kosciol/places.jsx
@@ -4,81 +4,102 @@ import Image from 'next/image'
 const Places = () => {
   return (
     <div className='w-full flex flex-col px-[5vw] my-[5vh] bg-white'>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
-            <div className='w-1/2 h-full relative'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
                 <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/nawa.jpeg"/>
             </div>
-            <div className="pl-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Nawa Glowna</h2>
+            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Nawa Główna</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/nawa.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             
-            <div className="pr-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Nawa Boczna (chór)</h2>
+            <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Nawa Boczna (chór)</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/boczna.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
-            <div className='w-1/2 h-full relative'>
-                <Image fill objectFit='cover' className='pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/boczna.jpeg"/>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
+                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/boczna.jpeg"/>
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
-            <div className='w-1/2 h-full relative'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
                 <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/oltarz.jpeg"/>
             </div>
-            <div className="pl-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Oltarz</h2>
+            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Oltarz</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/oltarz.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             
-            <div className="pr-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Dolny Kosciol</h2>
+            <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Dolny Kosciol</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/piwnica.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
-            <div className='w-1/2 h-full relative'>
-                <Image fill objectFit='cover' className='pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/piwnica.jpeg"/>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
+                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/piwnica.jpeg"/>
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
-            <div className='w-1/2 h-full relative'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
                 <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ambona.jpeg"/>
             </div>
-            <div className="pl-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Ambona</h2>
+            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Ambona</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ambona.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             
-            <div className="pr-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Zakrystia</h2>
+            <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Zakrystia</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/zakrystia.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
-            <div className='w-1/2 h-full relative'>
-                <Image fill objectFit='cover' className='pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/zakrystia.jpeg"/>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
+                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/zakrystia.jpeg"/>
             </div>
         </div>
-        <div className='flex flex-row w-full h-[50vh] my-[5vh]'>
-            <div className='w-1/2 h-full relative'>
+        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
+            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
                 <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/niebo.jpeg"/>
             </div>
-            <div className="pl-[5vw] w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2'>Niebo</h2>
+            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Niebo</h2>
+                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/niebo.jpeg"/>
+                </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
