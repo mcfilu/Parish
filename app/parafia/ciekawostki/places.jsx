90485a1a589cf3c3d8e33b137da12d31d055ebbe
mcfilu <filip.1olszewski@gmail.com>
2024-01-19 22:21:43 +0100

    updating the ciekawostki page to have itsown unique content, before it was copied from kosciol page

--- app/parafia/ciekawostki/places.jsx
+++ app/parafia/ciekawostki/places.jsx
@@ -6,12 +6,12 @@ const Places = () => {
     <div className='w-full flex flex-col px-[5vw] my-[5vh] bg-white'>
         <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/nawa.jpeg"/>
+                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki1.jpg"/>
             </div>
             <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Nawa Główna</h2>
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Coroczna Stajenka</h2>
                 <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/nawa.jpeg"/>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki1.jpg"/>
                 </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
@@ -21,26 +21,26 @@ const Places = () => {
         <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             
             <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Nawa Boczna (chór)</h2>
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Kwiaty przy Obrazie</h2>
                 <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/boczna.jpeg"/>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki2.jpg"/>
                 </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
             <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/boczna.jpeg"/>
+                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki2.jpg"/>
             </div>
         </div>
         <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/oltarz.jpeg"/>
+                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki3.jpg"/>
             </div>
             <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Oltarz</h2>
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Wystrój Ołtarza</h2>
                 <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/oltarz.jpeg"/>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki3.jpg"/>
                 </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
@@ -50,61 +50,19 @@ const Places = () => {
         <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
             
             <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Dolny Kosciol</h2>
+                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Prezenty Caritasu</h2>
                 <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/piwnica.jpeg"/>
+                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki4.jpg"/>
                 </div>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
                 
             </div>
             <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/piwnica.jpeg"/>
+                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ciekawostki4.jpg"/>
             </div>
         </div>
-        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
-            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/ambona.jpeg"/>
-            </div>
-            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Ambona</h2>
-                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/ambona.jpeg"/>
-                </div>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                
-            </div>
-        </div>
-        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
-            
-            <div className="pr-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Zakrystia</h2>
-                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/zakrystia.jpeg"/>
-                </div>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                
-            </div>
-            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='md:pl-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/zakrystia.jpeg"/>
-            </div>
-        </div>
-        <div className='flex flex-col md:flex-row w-full  my-[5vh]'>
-            <div className='hidden md:flex w-full md:w-1/2 h-[50vh] relative'>
-                <Image fill objectFit='cover' className='pr-[5vw]' alt="zdjecie ilustrujace miejsce w kosciele" src="/niebo.jpeg"/>
-            </div>
-            <div className="md:pl-[5vw] w-full md:w-1/2 h-full flex flex-col justify-center">
-                <h2 className='text-[4.5vh] font-header2 text-center md:text-left'>Niebo</h2>
-                <div className='flex md:hidden w-full md:w-1/2 h-[40vh] relative my-[1vh]'>
-                    <Image fill objectFit='cover' className='' alt="zdjecie ilustrujace miejsce w kosciele" src="/niebo.jpeg"/>
-                </div>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <p className='pt-[2vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                
-            </div>
-        </div>
+        
     </div>
   )
 }
