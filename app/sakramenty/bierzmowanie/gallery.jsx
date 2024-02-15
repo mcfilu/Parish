48f259d4a4068114950ada5fde3c363e1779fd8b
mcfilu <filip.1olszewski@gmail.com>
2024-02-15 17:15:34 +0100

    adding the descriptions and gallery to some of the bierzmowanie page components

--- app/sakramenty/bierzmowanie/gallery.jsx
+++ app/sakramenty/bierzmowanie/gallery.jsx
@@ -4,20 +4,20 @@ const Gallery = () => {
   return (
     <div className='w-full px-[5vw] py-[10vh] grid grid-cols-2 h-[90vh] gap-4 bg-white'>
         <div className='relative h-full w-full'>
-            <Image src="/chrzest_gallery1.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            <Image src="/bierz1.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
         </div>
         <div className='grid grid-cols-2 h-full gap-4'>
             <div className='relative'>
-                <Image src="/chrzest_gallery2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/bierz2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/bierz3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/bierz4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/chrzest_gallery5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/bierz5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
 
         </div>
--- app/sakramenty/bierzmowanie/intro.jsx
+++ app/sakramenty/bierzmowanie/intro.jsx
@@ -4,13 +4,13 @@ const Intro = () => {
   return (
     <div className='flex flex-row w-full h-[70vh] px-[5vw] bg-white my-[5vh]'>
         <div className='h-full w-1/2  relative'>
-            <Image className='pr-[5vw]' fill objectFit='cover' src="/chrzest2.jpeg" alt="chrzest"/>
+            <Image className='pr-[5vw]' fill objectFit='cover' src="/bierz6.webp" alt="chrzest"/>
         </div>
         <div className="h-full w-1/2 flex flex-col justify-center">
-            <h2 className='text-[4.5vh] font-header2'>Long heading is what you see here in this feature section</h2>
-            <p className='mt-[5vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
-            <p className='mt-[3vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
-            <p className='mt-[3vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
+            <h2 className='text-[4.5vh] font-header2'>Znaczenie Sakramentu</h2>
+            <p className='mt-[5vh]'>„Przyjmij znamię daru Ducha Świętego...” . Umocnienie darami Ducha Świętego do dawania świadectwa wierze - głównie przykładem swego życia, ważniejszym od wszelkich słów .</p>
+            <p className='mt-[3vh]'>Od chwili chrztu, odrodzony z wody i Ducha Świętego, wchodzi w skład rodziny dzieci Bożych, staje się chrześcijaninem, członkiem Kościoła. Potem dorasta i sam może powiedzieć swoje «tak» Bogu, «tak» dobrowolne i świadome. Sakrament bierzmowania potwierdza chrzest. Teraz uczeń Chrystusa pełen wdzięczności, może przyjąć Jego wielkie dary, które pomagają na drodze życia, aby stał się wiernym i odważnym świadkiem Jezusa. Dary Ducha Świętego pozwalają kształtować siebie jako chrześcijan, żyć Ewangelią i być aktywnymi członkiem wspólnoty.</p>
+            {/* <p className='mt-[3vh]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p> */}
         </div>
     </div>
   )
