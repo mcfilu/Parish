6a4a477235b6978629a30aba3c9dbaab74e89b11
mcfilu <filip.1olszewski@gmail.com>
2024-02-15 23:46:51 +0100

    changing the bg color to something greenish

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,7 @@ module.exports = {
         // gold2: "#d9c61c",
         gold2: "#c7a94c",
         // dark: "#0D2C4B",
-        dark: "#212121",
+        dark: "#607274",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
