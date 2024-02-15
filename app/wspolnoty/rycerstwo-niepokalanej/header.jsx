e0efb098e1404a7a18b76409212f8b5c88b5d39e
mcfilu <filip.1olszewski@gmail.com>
2024-02-15 14:41:13 +0100

    adding content into the rycerstwo niepokalanej page

--- app/wspolnoty/rycerstwo-niepokalanej/header.jsx
+++ app/wspolnoty/rycerstwo-niepokalanej/header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <div className='flex flex-col w-full h-screen'>
         <div className='w-full h-full relative'>
-            <Image alt="ksiadz proboszcz kazanie" fill objectFit='cover' src="/schola1.jpeg"/>
+            <Image alt="ksiadz proboszcz kazanie" fill objectFit='cover' src="/ryc0.jpeg"/>
         </div>
         <div className='bg-white w-full flex flex-col md:flex-row px-[5vw] md:py-[2vh] pt-[0] pb-[1vh] items-start md:items-center justify-between'>
-            <h1 className='text-[55px] font-header2 font-bold text-black'>Schola Parafialna</h1>
+            <h1 className='text-[55px] font-header2 font-bold text-black'>Rycerstwo Niepokalanej</h1>
             <div className='w-full md:w-[50%] h-full flex flex-col justify-center items-start'>
-                <p className='text-black text-[1.7vh]'>„Chrzcić” – oznacza zanurzać w wodzie. Każdy ochrzczony zostaje zanurzony w śmierci Chrystusa, z której powstaje przez zmartwychwstanie z Nim jako „nowe stworzenie” (2 Kor 5,17).</p>
+                <p className='text-black text-[1.7vh]'>"Kocham i pozdrawiam Ciebie, Matko, która jesteś w niebie."</p>
                 <button className='bg-dark text-white px-4 py-3 mt-[1vh]'>Zobacz Więcej</button>
             </div>
         </div>
--- app/wspolnoty/rycerstwo-niepokalanej/intro.jsx
+++ app/wspolnoty/rycerstwo-niepokalanej/intro.jsx
@@ -4,19 +4,19 @@ const Intro = () => {
   return (
     <div className='flex flex-col md:flex-row w-full py-[5vh] md:py-[10vh] px-[5vw] bg-white'>
         <div className='w-1/2  relative'>
-            <Image className='pr-[5vw]' fill objectFit='cover' src="/schola4.jpeg" alt="schola"/>
+            <Image className='pr-[5vw]' fill objectFit='cover' src="/ryc1.jpeg" alt="schola"/>
         </div>
         <div className="h-full w-full md:w-1/2 flex flex-col justify-center">
             <h2 className='text-[4.5vh] font-header2'>Kim jesteśmy i jak działamy?</h2>
-            <p className='mt-[5vh]'>Scholę założyła dwójka muzycznych pasjonatów Marysia Osińska (obecnie Dobrowolska) i Tomek Owczarek. Schola powstała dokładnie 17 lutego 2007 roku, kiedy to na pierwszą próbę przyszło łącznie 5 osób. Od tamtej pory skład zmieniał się wielokrotnie i tak trwamy do dzisiaj śpiewając na niedzielnej mszy świętej o godz. 18.00</p>
-            <p className='mt-[2vh]'>Zakładaliśmy, że będzie to schola studencka. Praktyka jednak pokazała, że nasza wspólnota potrzebuje bardziej różnorodnego zespołu śpiewaków. I tak w naszym składzie do dzisiaj znajdują swoje miejsce studenci, już nie-studenci, inżynierowie, lekarze, rodzice, matki, ojcowie, pedagodzy, finansiści, stolarze itd.</p>
-            <p className='mt-[2vh]'>Naszym zadaniem jest przede wszystkim wzbogacenie oprawy mszy świętych, adoracji, medytacji różańcowych i świąt uroczyście celebrowanych w naszej parafii. Biorąc pod uwag fakt, że śpiew pomaga się modlić i sam w sobie może być modlitwą oraz fakt, że człowiek nie żyje sam dla siebie, nasza służba polega na pomocy innym w poznaniu i przybliżeniu się do Boga, a przynajmniej stworzeniu atmosfery, która to umożliwi.</p>
-            <p className='mt-[2vh]'>Nie śpiewamy dla siebie – to pomaga zrozumieć nasze miejsce w Kościele.</p>
-            <p className='mt-[2vh]'>W naszym repertuarze są liturgiczne i religijne pieśni dominikańskie, tradycyjne, jak i współczesnych kompozytorów.</p>
-            <p className='mt-[2vh]'>Śpiewamy również „na zaproszenie” na różnych uroczystościach (np. śluby, pierwsze komunie, pogrzeby). Zebrane fundusze przeznaczamy wyłącznie na nasz rozwój – szkoląc nasze głosy pod kierownictwem specjalistów z Uniwersytetu Muzycznego im. Fryderyka Chopina lub na warsztatach organizowanych przez inne chóry i parafie.</p>
+            <p className='mt-[5vh]'>Rycerstwo Niepokalanej, będące wyrazem oddania Matce Bożej Niepokalanej, tworzy niezwykłą wspólnotę, której członkowie kierują swoje starania ku nawróceniu i uświęceniu swojego życia. Po przeżytym nawróceniu i podjęciu osobistego procesu uświęcenia, wypełniają swoje serca misją Kościoła, adaptując ją do swojego życia codziennego.</p>
+            <p className='mt-[2vh]'>Ich pragnienie nie ogranicza się do wewnętrznej przemiany, ale rozszerza się na nieśenie Ewangelii Chrystusa na szerokie wody społeczeństwa, stając się źródłem nadziei i odnowy dla ludzi. Jako rycerze Niepokalanej, czują się wezwani do głoszenia tej Ewangelii jako drogi prowadzącej do zbawienia i pełni życia, pragnąc przywracać wiarę i nadzieję w sercach wielu.</p>
+            <p className='mt-[2vh]'>W ich służbie dla Matki Bożej, inspirują innych do podążania drogą świętości i poświęcenia, czyniąc ze swojego życia i działalności prawdziwe świadectwo miłości Bożej.</p>
+            {/* <p className='mt-[2vh]'>Nie śpiewamy dla siebie – to pomaga zrozumieć nasze miejsce w Kościele.</p> */}
+            {/* <p className='mt-[2vh]'>W naszym repertuarze są liturgiczne i religijne pieśni dominikańskie, tradycyjne, jak i współczesnych kompozytorów.</p> */}
+            {/* <p className='mt-[2vh]'>Śpiewamy również „na zaproszenie” na różnych uroczystościach (np. śluby, pierwsze komunie, pogrzeby). Zebrane fundusze przeznaczamy wyłącznie na nasz rozwój – szkoląc nasze głosy pod kierownictwem specjalistów z Uniwersytetu Muzycznego im. Fryderyka Chopina lub na warsztatach organizowanych przez inne chóry i parafie.</p> */}
         </div>
         <div className='md:hidden h-[40vh] w-full mt-[5vh] relative'>
-            <Image className='pr-[5vw]' fill objectFit='cover' src="/schola4.jpeg" alt="schola"/>
+            <Image className='pr-[5vw]' fill objectFit='cover' src="/ryc1.jpeg" alt="schola"/>
         </div>
     </div>
   )
