6663082dacfc160a2ab772f8112823220b62f12e
mcfilu <filip.1olszewski@gmail.com>
2024-02-15 13:40:40 +0100

    updating the caritas page to nearly final look with images and texts

--- app/wspolnoty/caritas/banner.jsx
+++ app/wspolnoty/caritas/banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
          <h2 className='text-[5.5vh] font-header2 text-center'>Informacje Kontaktowe</h2>
         <div className='flex flex-col md:flex-row w-full justify-between items-center mt-[5vh]'>
         <div className='w-full md:w-[40%] flex flex-col'>
-          <p className='text-white text-[1.7vh]'>Próby scholii odbywają się na plebanii (domofon SALA SPOTKAŃ):</p>
-          <p className='text-white text-[1.7vh] mt-[1vh]'>- w czwartki o godz. 19.00</p>
-          <p className='text-white text-[1.7vh]'>- w niedzielę o godz. 17.00 (przed mszą św.).</p>
+          <p className='text-white text-[1.7vh]'>Osoby pragnące skorzystać z różnych form pomocy PZC, jak i osoby pragnące służyć swoją pomocą, proszone są o zgłaszanie do diakona Cezarego Uszyńskiego:</p>
+          <p className='text-white text-[1.7vh] mt-[1vh]'>- tel. 605 074 413 (dostępny stale, w razie nieodebrania oddzwania)</p>
+          <p className='text-white text-[1.7vh]'>- osobiście w Diakonii na plebanii w poniedziałki w godz. 15 30 – 17 30</p>
         </div>
         <div className='w-full md:w-[40%] mt-[3vh] md:mt-0 flex flex-col'>
-          <p className='text-white text-[1.7vh]'>Jeżeli jesteś pasjonatem śpiewania, to zapraszamy do nas.</p>
-          <p className='text-white text-[1.7vh] mt-[1vh]'>KONTAKT: Tomasz Owczarek tel. +48 501 561 393 (tomaszadamowczarek@gmail.com)</p>
+          <p className='text-white text-[1.7vh]'>Miłosierdzie okazywane ubogim, chorym, starszym i samotnym w ramach parafialnej Caritas, to szansa dla każdej i każdego z nas praktykowania miłości bliźniego, stanowiącej zasadę naszej Wiary. Konkretne czyny są naszą odpowiedzią na przykazanie miłości bliźniego. Świadczą o naszej dojrzałości chrześcijańskiej.</p>
+          <p className='text-white text-[1.7vh] mt-[1vh]'>- wrzucając karteczkę ze swoim numerem telefonu do skarbony przy Św. Janie Pawle II w kościele</p>
         </div>
         </div>
     </div>
--- app/wspolnoty/caritas/intro.jsx
+++ app/wspolnoty/caritas/intro.jsx
@@ -4,19 +4,19 @@ const Intro = () => {
   return (
     <div className='flex flex-col md:flex-row w-full py-[5vh] md:py-[10vh] px-[5vw] bg-white'>
         <div className='w-1/2  relative'>
-            <Image className='pr-[5vw]' fill objectFit='cover' src="/schola4.jpeg" alt="schola"/>
+            <Image className='pr-[5vw]' fill objectFit='cover' src="/caritas6.jpeg" alt="schola"/>
         </div>
         <div className="h-full w-full md:w-1/2 flex flex-col justify-center">
             <h2 className='text-[4.5vh] font-header2'>Kim jesteśmy i jak działamy?</h2>
-            <p className='mt-[5vh]'>Scholę założyła dwójka muzycznych pasjonatów Marysia Osińska (obecnie Dobrowolska) i Tomek Owczarek. Schola powstała dokładnie 17 lutego 2007 roku, kiedy to na pierwszą próbę przyszło łącznie 5 osób. Od tamtej pory skład zmieniał się wielokrotnie i tak trwamy do dzisiaj śpiewając na niedzielnej mszy świętej o godz. 18.00</p>
-            <p className='mt-[2vh]'>Zakładaliśmy, że będzie to schola studencka. Praktyka jednak pokazała, że nasza wspólnota potrzebuje bardziej różnorodnego zespołu śpiewaków. I tak w naszym składzie do dzisiaj znajdują swoje miejsce studenci, już nie-studenci, inżynierowie, lekarze, rodzice, matki, ojcowie, pedagodzy, finansiści, stolarze itd.</p>
-            <p className='mt-[2vh]'>Naszym zadaniem jest przede wszystkim wzbogacenie oprawy mszy świętych, adoracji, medytacji różańcowych i świąt uroczyście celebrowanych w naszej parafii. Biorąc pod uwag fakt, że śpiew pomaga się modlić i sam w sobie może być modlitwą oraz fakt, że człowiek nie żyje sam dla siebie, nasza służba polega na pomocy innym w poznaniu i przybliżeniu się do Boga, a przynajmniej stworzeniu atmosfery, która to umożliwi.</p>
-            <p className='mt-[2vh]'>Nie śpiewamy dla siebie – to pomaga zrozumieć nasze miejsce w Kościele.</p>
-            <p className='mt-[2vh]'>W naszym repertuarze są liturgiczne i religijne pieśni dominikańskie, tradycyjne, jak i współczesnych kompozytorów.</p>
-            <p className='mt-[2vh]'>Śpiewamy również „na zaproszenie” na różnych uroczystościach (np. śluby, pierwsze komunie, pogrzeby). Zebrane fundusze przeznaczamy wyłącznie na nasz rozwój – szkoląc nasze głosy pod kierownictwem specjalistów z Uniwersytetu Muzycznego im. Fryderyka Chopina lub na warsztatach organizowanych przez inne chóry i parafie.</p>
+            <p className='mt-[5vh]'>Nasz Parafialny Zespół Caritas powołano specjalnym Aktem w dniu 2.10.2002 r. Przez blisko dwadzieścia lat prowadził różne formy wsparcia: refundację leków, dofinansowanie wypoczynku dzieci, zakup sprzętu rehabilitacyjnego, paczki świąteczne, pomoc rodzinom wielodzietnym, ludziom starszym i samotnym.</p>
+            <p className='mt-[2vh]'>Okres pandemii był czasem ograniczeń. Mała liczba wiernych w kościele, zaniechanie zbiórek pieniężnych oraz brak bezpośrednich kontaktów zahamowały także działalność P.Z.C.</p>
+            <p className='mt-[2vh]'>Dzięki wrażliwości i chrześcijańskiej postawie niektórych parafian udało się kontynuować działalność pomocową. Warto odnotować zarówno odwagę, jak i ofiarność z jaką nieśli oni pomoc, nie szczędząc czasu i swoich własnych środków. To piękna karta historii Parafialnej Caritas Parafii Św. Andrzeja Apostoła w Warszawie.</p>
+            <p className='mt-[2vh]'>W ostatnim okresie pojawiło się u wielu osób w naszej parafialnej społeczności, zwłaszcza starszych, nowe zjawisko, polegające na przyzwyczajeniu się do przeżywania swojej wiary bez uczestnictwa we Mszy św. i bez korzystania z sakramentów. Pomimo braku obiektywnych przyczyn zdrowotnych osoby te zadowalają się transmisją telewizyjną Mszy św., pomimo braku stosownej dyspensy biskupa. Izolują się od wspólnoty, stając się samotnymi na własne życzenie. Nie możemy ich zostawić, powinniśmy temu trendowi przeciwdziałać. Jest to zadanie także dla Parafialnej Caritas.</p>
+            <p className='mt-[2vh]'>Aktualnie dzięki zbiórkom ofiar do puszek w każdą pierwszą niedzielę miesiąca, prowadzimy pomoc dla osób potrzebujących. Refundujemy zakup leków, finansujemy zakup niezbędnej odzieży i żywności. Pomagamy przy zaopatrzeniu przed świętami i na co dzień. Odwiedzamy osoby, starsze, chore i samotne rozeznając każdą indywidualną sytuację.</p>
+            <p className='mt-[2vh]'>Rodzą się pomysły służące integracji i budowaniu wspólnoty. Caritas rozumiana jako miłość bliźniego łączy się z diakonią, jako postawą służby bliźniemu, którą jesteśmy zobowiązani w sobie budować. Jako uczniowie Chrystusa, do których pragniemy być zaliczeni, postępujemy za Nim. Chcemy naśladować w posłudze Miłości Mistrza, który przyszedł na świat by służyć, a nie by Mu służono. Zapraszamy do działania. Jest potrzeba wzmocnienia wolontariatu oraz kierownictwa PZC.</p>
         </div>
         <div className='md:hidden h-[40vh] w-full mt-[5vh] relative'>
-            <Image className='pr-[5vw]' fill objectFit='cover' src="/schola4.jpeg" alt="schola"/>
+            <Image className='pr-[5vw]' fill objectFit='cover' src="/caritas6.jpeg" alt="schola"/>
         </div>
     </div>
   )
