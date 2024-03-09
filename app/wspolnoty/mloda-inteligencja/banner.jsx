04571d7f3309a9f647bc21f75675d9882d0a9388
mcfilu <filip.1olszewski@gmail.com>
2024-03-09 16:47:48 +0000

    new contact banner kim

--- app/wspolnoty/mloda-inteligencja/banner.jsx
+++ app/wspolnoty/mloda-inteligencja/banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
          <h2 className='text-[5.5vh] font-header2 text-center'>Informacje Kontaktowe</h2>
         <div className='flex flex-col md:flex-row w-full justify-between items-center mt-[5vh]'>
         <div className='w-full md:w-[40%] flex flex-col'>
-          <p className='text-white text-[1.7vh]'>Próby scholii odbywają się na plebanii (domofon SALA SPOTKAŃ):</p>
-          <p className='text-white text-[1.7vh] mt-[1vh]'>- w czwartki o godz. 19.00</p>
-          <p className='text-white text-[1.7vh]'>- w niedzielę o godz. 17.00 (przed mszą św.).</p>
+          <p className='text-white text-[1.7vh]'>Na razie zbieramy się nieregularnie, ale w planach mamy uruchomienie cyklicznych comiesięcznych spotkań. Serdecznie zapraszamy do współpracy.</p>
+          {/* <p className='text-white text-[1.7vh] mt-[1vh]'>- w czwartki o godz. 19.00</p> */}
+          {/* <p className='text-white text-[1.7vh]'>- w niedzielę o godz. 17.00 (przed mszą św.).</p> */}
         </div>
         <div className='w-full md:w-[40%] mt-[3vh] md:mt-0 flex flex-col'>
-          <p className='text-white text-[1.7vh]'>Jeżeli jesteś pasjonatem śpiewania, to zapraszamy do nas.</p>
-          <p className='text-white text-[1.7vh] mt-[1vh]'>KONTAKT: Tomasz Owczarek tel. +48 501 561 393 (tomaszadamowczarek@gmail.com)</p>
+          <p className='text-white text-[1.7vh]'>Kontakt KMJ@parafiaandrzeja.pl</p>
+          {/* <p className='text-white text-[1.7vh] mt-[1vh]'>KONTAKT: Tomasz Owczarek tel. +48 501 561 393 (tomaszadamowczarek@gmail.com)</p> */}
         </div>
         </div>
     </div>
