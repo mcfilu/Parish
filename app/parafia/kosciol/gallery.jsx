0ac8b29ffed8e87e1ff99bded6936e85c419304d
mcfilu <filip.1olszewski@gmail.com>
2024-03-10 00:11:03 +0000

    change kosciol agallery imgages|

--- app/parafia/kosciol/gallery.jsx
+++ app/parafia/kosciol/gallery.jsx
@@ -33,14 +33,14 @@ const Gallery = () => {
   return (
     <div className='w-full px-[5vw] grid grid-cols-1 md:grid-cols-2 h-[90vh] py-[10vh] md:h-[80vh] gap-4 bg-white'>
         <div className='relative h-full w-full'>
-            <Image src="/kosciol5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+            <Image src="/koscioldzisiaj1.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
         </div>
         <div className='grid grid-cols-2 h-full gap-4'>
             <div className='relative'>
                 <Image src="/kosciol4.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
-                <Image src="/kosciol3.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
+                <Image src="/kosciol5.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
             </div>
             <div className='relative h-full w-full'>
                 <Image src="/kosciol2.jpeg" alt="zdjecie schola" fill objectFit='cover'/>
