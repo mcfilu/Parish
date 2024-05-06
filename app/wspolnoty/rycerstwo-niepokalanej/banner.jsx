6c2c5ce2238ca7dbe2c2449031b4a2a75efaff1a
mcfilu <filip.1olszewski@gmail.com>
2024-05-06 20:29:07 +0100

    zmiana niepokolana 1 na pierszą"

--- app/wspolnoty/rycerstwo-niepokalanej/banner.jsx
+++ app/wspolnoty/rycerstwo-niepokalanej/banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className='flex flex-col justify-between w-full px-[5vw]  bg-dark text-white items-center py-[5vh]'>
          <h2 className='text-[5.5vh] font-header2 text-center'>Informacje Kontaktowe</h2>
-         <p className='text-white text-[1.7vh] mt-[5vh]'>Rycerstwo Niepokalnej spotyka się na wspólnej Eucharystii  i modlitwie przed wystawionym Najświętszym Sakramentem, w każdą I sobotę miesiąca o godz. 9.00 </p>
+         <p className='text-white text-[1.7vh] mt-[5vh]'>Rycerstwo Niepokalnej spotyka się na wspólnej Eucharystii  i modlitwie przed wystawionym Najświętszym Sakramentem, w każdą pierwszą sobotę miesiąca o godz. 9.00 </p>
         {/* <div className='flex flex-col md:flex-row w-full justify-between items-center mt-[5vh]'>
         <div className='w-full md:w-[40%] flex flex-col'>
           <p className='text-white text-[1.7vh]'>Próby scholii odbywają się na plebanii (domofon SALA SPOTKAŃ):</p>
