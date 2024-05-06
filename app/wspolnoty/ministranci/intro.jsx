91ee548a8ffb27bd9cee49806f14585c4bdb0d65
mcfilu <filip.1olszewski@gmail.com>
2024-05-06 20:25:53 +0100

    wszystekie sekcje wspolnot poprawienie czcionki, nie bylo dynamic size

--- app/wspolnoty/ministranci/intro.jsx
+++ app/wspolnoty/ministranci/intro.jsx
@@ -8,8 +8,8 @@ const Intro = () => {
         </div>
         <div className="h-full w-full md:w-1/2 flex flex-col justify-center">
             <h2 className='text-[4.5vh] font-header2'>Kim jesteśmy i jak działamy?</h2>
-            <p className='mt-[5vh]'>Ministranci - służą Bogu i ludziom, to młodzi ludzie którzy za cel obrali sobie być najbliżej ołtarza. Służą do Mszy św. i nabożeństw w kościele, lektorzy czytają Słowo Boże, pomagają w przygotowaniu rożnych dekoracji i uroczystości kościelnych.</p>
-            <p className='mt-[2vh]'>Pogłębiają swoją formację poprzez cotygodniowe spotkania w sobotę o godz. 11.00 na plebanii. Tworzą wspólnotę braterstwa poprzez wspólne zabawy i wycieczki - kręgle, strzelnica, obozy wędrowne.</p>
+            <p className='mt-[5vh] text-[1.7vh] mt-[1vh]'>Ministranci - służą Bogu i ludziom, to młodzi ludzie którzy za cel obrali sobie być najbliżej ołtarza. Służą do Mszy św. i nabożeństw w kościele, lektorzy czytają Słowo Boże, pomagają w przygotowaniu rożnych dekoracji i uroczystości kościelnych.</p>
+            <p className='mt-[2vh] text-[1.7vh] mt-[1vh]'>Pogłębiają swoją formację poprzez cotygodniowe spotkania w sobotę o godz. 11.00 na plebanii. Tworzą wspólnotę braterstwa poprzez wspólne zabawy i wycieczki - kręgle, strzelnica, obozy wędrowne.</p>
             {/* <p className='mt-[2vh]'>Naszym zadaniem jest przede wszystkim wzbogacenie oprawy mszy świętych, adoracji, medytacji różańcowych i świąt uroczyście celebrowanych w naszej parafii. Biorąc pod uwag fakt, że śpiew pomaga się modlić i sam w sobie może być modlitwą oraz fakt, że człowiek nie żyje sam dla siebie, nasza służba polega na pomocy innym w poznaniu i przybliżeniu się do Boga, a przynajmniej stworzeniu atmosfery, która to umożliwi.</p> */}
             {/* <p className='mt-[2vh]'>Nie śpiewamy dla siebie – to pomaga zrozumieć nasze miejsce w Kościele.</p> */}
             {/* <p className='mt-[2vh]'>W naszym repertuarze są liturgiczne i religijne pieśni dominikańskie, tradycyjne, jak i współczesnych kompozytorów.</p> */}
