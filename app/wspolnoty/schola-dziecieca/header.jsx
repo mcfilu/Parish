91679257bfb38284b6fc7d2421cbc85c9f747b86
mcfilu <filip.1olszewski@gmail.com>
2024-05-20 23:26:34 +0100

    dodanie funkcjonalnosc do zobacz wiecej button schol dz dla proby

--- app/wspolnoty/schola-dziecieca/header.jsx
+++ app/wspolnoty/schola-dziecieca/header.jsx
@@ -11,7 +11,9 @@ const Header = () => {
             <h1 className='text-[55px] font-header2 font-bold text-black'>Schola Rodzinna</h1>
             <div className='w-full md:w-[50%] h-full flex flex-col justify-center items-start'>
                 <p className='text-black text-[1.7vh]'>&ldquo;Gdzie słyszysz śpiew tam idź, tam dobre serca mają. Źli ludzie wierzaj mi, Ci nigdy nie śpiewają.&rdquo; Johann Wolfgang Goethe</p>
+                <a href="#first">
                 <button className='bg-dark text-white px-4 py-3 mt-[1vh]'>Zobacz więcej</button>
+                </a>
             </div>
         </div>
     </div>
