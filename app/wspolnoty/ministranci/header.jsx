f313a9a50b318a3a90eb2f41f592e65b35414005
mcfilu <filip.1olszewski@gmail.com>
2024-05-20 20:36:18 +0100

    zmiana Jan Pawel II plus spacja przed przecinkiem

--- app/wspolnoty/ministranci/header.jsx
+++ app/wspolnoty/ministranci/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className='bg-white w-full flex flex-col md:flex-row px-[5vw] md:py-[2vh] pt-[0] pb-[1vh] items-start md:items-center justify-between'>
             <h1 className='text-[55px] font-header2 font-bold text-black'>Służba Liturgiczna</h1>
             <div className='w-full md:w-[50%] h-full flex flex-col justify-center items-start'>
-                <p className='text-black text-[1.7vh]'>&ldquo;Kiedy mali chłopcy i młodzieńcy pełnią służbę ołtarza z radością i entuzjazmem , dają swoim rówieśnikom wymowne świadectwo doniosłości i piękna Eucharystii&rdquo; (JAN PAWEŁ II)</p>
+                <p className='text-black text-[1.7vh]'>&ldquo;Kiedy mali chłopcy i młodzieńcy pełnią służbę ołtarza z radością i entuzjazmem, dają swoim rówieśnikom wymowne świadectwo doniosłości i piękna Eucharystii&rdquo; Jan Paweł II</p>
                 <button className='bg-dark text-white px-4 py-3 mt-[1vh]'>Zobacz Więcej</button>
             </div>
         </div>
