e1faada2b82d502d6cddb53f5d2d6e4e2420bbf4
mcfilu <filip.1olszewski@gmail.com>
2024-05-20 21:51:00 +0100

    dodanie ks dr l slipek do intro

--- app/components/home/intro.jsx
+++ app/components/home/intro.jsx
@@ -14,6 +14,8 @@ const Intro = () => {
             <p className='text-black text-[1.8vh] mt-[3vh]'>Parafia to dom wszystkich, to namiot Boga ustawiony pośród zwyczajnych ludzi, brama, która otwarta jest dla każdego. </p>
             <p className='text-black text-[1.8vh] mt-[1vh]'>Parafia ma być wspólnotą otwartą, w której każdy czuje się jak w domu, w której jest miejsce dla wszystkich, w której nikt nie zachowuje się jak pan i władca (bo posiadł tajemnicę).</p>
             <p className='text-black text-[1.8vh] mt-[1vh]'>Widzę parafię jako wspólnotę, w której każdy należący do niej powinien czuć się jak we własnym domu. Parafia ma być właśnie takim domem, który wypełnia wielka rodzina dzieci, młodzieży, dorosłych i osób starszych.</p>
+
+            <p className='text-black text-[3vh] font-header2'>ks. dr L. Slipek</p>
             
             <Link href="/parafia/kosciol"><button className='p-4  bg-dark text-white mt-[2vh] md:mt-[5vh]'>Zobacz Więcej</button></Link>
         </div>
