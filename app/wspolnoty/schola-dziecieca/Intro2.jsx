12097dd52ffbbaeb2f40330351a7bd5854c211ee
mcfilu <filip.1olszewski@gmail.com>
2024-05-20 23:24:33 +0100

    finalne zmiany intro2 schola dz

--- app/wspolnoty/schola-dziecieca/Intro2.jsx
+++ app/wspolnoty/schola-dziecieca/Intro2.jsx
@@ -4,13 +4,11 @@ import Image from 'next/image'
 const Intro2 = () => {
   return (
     <div className='flex flex-col md:flex-row  w-full py-[5vh] md:py-[10vh] px-[5vw] bg-white'>
-        <div className='w-1/2 relative hidden md:flex'>
-                <Image className='pr-[5vw]' fill objectFit='cover' src="/rodz3.jpeg" alt="schola"/>
-        </div>
+        
         
         <div className='flex flex-col w-full md:w-1/2 justify-center items-center mt-[5vh]'>
             <h2 className='text-[5.5vh] font-header2 text-center'>A na „szkole” śpiewania od kuchni…</h2>
-            <div className='w-full flex-col'>
+            <div className='w-full flex-col mt-[4vh]'>
                 <div className='w-full  flex flex-col'>
                     <p className='text-black text-[1.7vh]'>Przed:</p>
                     <p className='text-black text-[1.7vh] mt-[1vh]'>- przychodzimy punktualnie, chyba, że ktoś lubi przysiady …</p>
@@ -24,6 +22,9 @@ const Intro2 = () => {
                 </div> 
             </div>
             
+        </div>
+        <div className='w-1/2 relative hidden md:flex'>
+                <Image className='pl-[5vw]' fill objectFit='cover' src="/rodz3.jpeg" alt="schola"/>
         </div>
         <div className='md:hidden h-[40vh] w-full mt-[5vh] relative'>
             <Image className='pl-[5vw]' fill objectFit='cover' src="/rodz3.jpeg" alt="schola"/>
